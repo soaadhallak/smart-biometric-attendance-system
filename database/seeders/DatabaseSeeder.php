404103818323077ef81d11cbbb081df4945c289a
soaadhallak <soaadhallak2@gmail.com>
2026-04-21 21:00:06 +0300

    Add seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesAndPermissionsSeeder::class,
             YearSeeder::class,
-            StudentSystemSeeder::class,
+            CourseSeeder::class
         ]);
 
         $user = User::firstOrCreate(
